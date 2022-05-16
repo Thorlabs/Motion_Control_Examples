99c8d0d8b5adc671a33de3f7b98e328870b70492
Max Callé <mcalle@thorlabs.com>
2022-05-16 12:31:58 +0100

    Added MFF, Separated BBD30X from BBD20X

---
+++ C++/Modular Rack/MFF101/MFF101.cpp
@@ -0,0 +1,83 @@
+// Example_MFF101.cpp : Defines the entry point for the console application.
+
+
+#include <stdlib.h>
+#include <conio.h>
+#include <stdio.h>
+
+#include "Thorlabs.MotionControl.FilterFlipper.h"
+
+
+int __cdecl wmain(int argc, wchar_t* argv[])
+{
+
+	int serialNo = 37837825;
+
+
+	// identify and access device
+	char testSerialNo[16];
+	sprintf_s(testSerialNo, "%d", serialNo);
+
+	// Build list of connected device
+	if (TLI_BuildDeviceList() == 0)
+	{
+		// get device list size 
+		short n = TLI_GetDeviceListSize();
+		// get MFF serial numbers
+		char serialNos[100];
+		TLI_GetDeviceListByTypeExt(serialNos, 100, 37);
+
+		// output list of matching devices
+		{
+			char *searchContext = nullptr;
+			char *p = strtok_s(serialNos, ",", &searchContext);
+
+			while (p != nullptr)
+			{
+				TLI_DeviceInfo deviceInfo;
+				// get device info from device
+				TLI_GetDeviceInfo(p, &deviceInfo);
+				// get strings from device info structure
+				char desc[65];
+				strncpy_s(desc, deviceInfo.description, 64);
+				desc[64] = '\0';
+				char serialNo[9];
+				strncpy_s(serialNo, deviceInfo.serialNo, 8);
+				serialNo[8] = '\0';
+				// output
+				printf("Found Device %s=%s : %s\r\n", p, serialNo, desc);
+				p = strtok_s(nullptr, ",", &searchContext);
+			}
+		}
+
+		// open device
+		if (FF_Open(testSerialNo) == 0)
+		{
+			// start the device polling at 200ms intervals
+			FF_StartPolling(testSerialNo, 200);
+
+			// NOTE The following uses Sleep functions to simulate timing
+			// In reality, the program should read the status to check that commands have been completed
+			Sleep(1000);
+
+			int position = FF_GetPosition(testSerialNo);
+			printf("Device %s current position is %d\r\n", testSerialNo, position);
+
+			// move to next position
+			FF_MoveToPosition(testSerialNo, position == 1 ? FF_Positions::Position2 : FF_Positions::Position1);
+			printf("Device %s moving\r\n", testSerialNo);
+			Sleep(1000);
+
+			position = FF_GetPosition(testSerialNo);
+			printf("Device %s current position is %d\r\n", testSerialNo, position);
+
+			// stop polling
+			FF_StopPolling(testSerialNo);
+			// close device
+			FF_Close(testSerialNo);
+		}
+	}
+
+	char c = _getch();
+	return 0;
+}

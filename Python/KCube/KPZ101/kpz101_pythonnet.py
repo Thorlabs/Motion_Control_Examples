bb50f0a1dab005cb1e7e8d0f7caad209216e45fc
Max Callé <95913120+ThorlabsMCalle@users.noreply.github.com>
2022-06-17 17:07:06 +0100

    KPZ101 Example

--- Python/KCube/KPZ101/kpz101_pythonnet.py
+++ Python/KCube/KPZ101/kpz101_pythonnet.py
@@ -1,8 +1,8 @@
 """
-pythonnet_template
+kpz101_pythonnet
 ==================
 
-A template useful for the creation of custom software for MotionControl Products.
+An example of using the .NET API with the pythonnet package for controlling a KPZ101
 """
 import os
 import time
@@ -11,10 +11,10 @@ import clr
 
 clr.AddReference("C:\\Program Files\\Thorlabs\\Kinesis\\Thorlabs.MotionControl.DeviceManagerCLI.dll")
 clr.AddReference("C:\\Program Files\\Thorlabs\\Kinesis\\Thorlabs.MotionControl.GenericMotorCLI.dll")
-clr.AddReference("C:\\Program Files\\Thorlabs\\Kinesis\\ThorLabs.MotionControl.<>.dll")
+clr.AddReference("C:\\Program Files\\Thorlabs\\Kinesis\\ThorLabs.MotionControl.KCube.PiezoCLI.dll")
 from Thorlabs.MotionControl.DeviceManagerCLI import *
 from Thorlabs.MotionControl.GenericMotorCLI import *
-# from Thorlabs.MotionControl.<> import *
+from Thorlabs.MotionControl.KCube.PiezoCLI import *
 from System import Decimal  # necessary for real world units
 
 def main():
@@ -31,7 +31,7 @@ def main():
         serial_no = "26000001"  # Replace this line with your device's serial number
 
         # Connect, begin polling, and enable
-        device = None
+        device = KCubePiezo.CreateKCubePiezo(serial_no)
 
         # Get Device Information and display description
         device_info = device.GetDeviceInfo()
@@ -47,7 +47,34 @@ def main():
             device.WaitForSettingsInitialized(10000)  # 10 second timeout
             assert device.IsSettingsInitialized() is True
 
+        # Load the device configuration
+        device_config = device.GetPiezoConfiguration(serial_no)
 
+        # This shows how to obtain the device settings
+        device_settings = device.PiezoDeviceSettings
+
+        # Set the Zero point of the device
+        print("Setting Zero Point")
+        device.SetZero()
+
+        # Get the maximum voltage output of the KPZ
+        max_voltage = device.GetMaxOutputVoltage()  # This is stored as a .NET decimal
+
+        # Go to a voltage
+        dev_voltage = Decimal(15.0)
+        print(f'Going to voltage {dev_voltage}')
+
+        if dev_voltage != 0 and dev_voltage <= max_voltage:
+            device.SetOutputVoltage(dev_voltage)
+            time.Sleep(1)
+
+            print(f'Moved to Voltage {device.GetOutputVoltage()}')
+        else:
+            print(f'Voltage must be between 0 and {max_voltage}')
+
+        # Stop Polling and Disconnect
+        device.StopPolling()
+        device.Disconnect()
     except Exception as e:
         print(e)
 

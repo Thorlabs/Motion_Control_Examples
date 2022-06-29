e90ea2b7643365addacfd29b3fb64238ca99211c
Max Callé <95913120+ThorlabsMCalle@users.noreply.github.com>
2022-06-29 12:02:13 +0100

    started modular rack python example

--- Python/KCube/KDC101/kdc101_pythonnet.py
+++ Python/KCube/KDC101/kdc101_pythonnet.py
@@ -25,43 +25,50 @@ def main():
 
         DeviceManagerCLI.BuildDeviceList()
 
-        kcube = KCubeDCServo.CreateKCubeDCServo(serial_no)
+        device = KCubeDCServo.CreateKCubeDCServo(serial_no)
 
         # Connect, begin polling, and enable
-        kcube.Connect(serial_no)
+        device.Connect(serial_no)
         time.sleep(0.25)
-        kcube.StartPolling(250)
+        device.StartPolling(250)
         time.sleep(0.25)  # wait statements are important to allow settings to be sent to the device
 
-        kcube.EnableDevice()
+        device.EnableDevice()
         time.sleep(0.25)  # Wait for device to enable
 
         # Get Device information
-        device_info = kcube.GetDeviceInfo()
+        device_info = device.GetDeviceInfo()
         print(device_info.Description)
 
         # Wait for Settings to Initialise
-        if not kcube.IsSettingsInitialized():
-            kcube.WaitForSettingsInitialized(10000)  # 10 second timeout
-            assert kcube.IsSettingsInitialized() is True
+        if not device.IsSettingsInitialized():
+            device.WaitForSettingsInitialized(10000)  # 10 second timeout
+            assert device.IsSettingsInitialized() is True
 
         # Before homing or moving device, ensure the motor's configuration is loaded
-        m_config = kcube.LoadMotorConfiguration(serial_no,
+        m_config = device.LoadMotorConfiguration(serial_no,
                                                 DeviceConfiguration.DeviceSettingsUseOptionType.UseDeviceSettings)
 
+        m_config.DeviceSettingsName = "MTS25"
+
+        m_config.UpdateCurrentConfiguration()
+
+        device_settings = device.MotorDeviceSettings
+        print(device_settings)
+
         print("Homing Actuator")
-        kcube.Home(60000)  # 10s timeout, blocking call
+        device.Home(60000)  # 10s timeout, blocking call
 
         print("Device Homed. Moving to position 5.0")
         f = 5.0
         d = Decimal(f)
-        kcube.MoveTo(d, 10000)  # 10s timeout again
+        device.MoveTo(d, 10000)  # 10s timeout again
         time.sleep(1)
 
-        print(f'Device now at position {kcube.Position}')
+        print(f'Device now at position {device.Position}')
         time.sleep(1)
 
-        kcube.Disconnect()
+        device.Disconnect()
     except Exception as e:
         print(e)
 
---
+++ Python/Modular Rack/mmr_pythonnet.py
@@ -0,0 +1,87 @@
+"""
+pythonnet_template
+==================
+
+A template useful for the creation of custom software for MotionControl Products.
+Originally written in a python 3.10.5 environment with a pre-release version of pythonnet (June 22)
+"""
+import os
+import time
+import sys
+import clr
+
+clr.AddReference("C:\\Program Files\\Thorlabs\\Kinesis\\Thorlabs.MotionControl.DeviceManagerCLI.dll")
+clr.AddReference("C:\\Program Files\\Thorlabs\\Kinesis\\Thorlabs.MotionControl.GenericMotorCLI.dll")
+clr.AddReference("C:\\Program Files\\Thorlabs\\Kinesis\\ThorLabs.MotionControl.ModularRackCLI.dll")
+from Thorlabs.MotionControl.DeviceManagerCLI import *
+from Thorlabs.MotionControl.GenericMotorCLI import *
+from Thorlabs.MotionControl.ModularRackCLI.Rack import *
+from System import Decimal  # necessary for real world units
+
+def main():
+    """The main entry point for the application"""
+
+    # Uncomment this line if you are using
+    # SimulationManager.Instance.InitializeSimulations()
+
+    try:
+
+        DeviceManagerCLI.BuildDeviceList()
+
+        # create new device
+        serial_no = "26000001"  # Replace this line with your device's serial number
+
+        # Get Device info using a factory
+        device_info = DeviceFactory.GetDeviceInfo(serial_no)
+        print(device_info.GetTypeID())
+
+        rack = ModularRack.CreateModularRack(int(DeviceInfo.GetTypeID()), serial_no)
+        # Connect, begin polling, and enable
+        device = rack[1]
+
+        rack.Connect(serial_no)
+
+        # Ensure that the device settings have been initialized
+        if not device.IsSettingsInitialized():
+            device.WaitForSettingsInitialized(10000)  # 10 second timeout
+            assert device.IsSettingsInitialized() is True
+
+        # Start polling and enable
+        device.StartPolling(250)  #250ms polling rate
+        time.sleep(25)
+        device.EnableDevice()
+        time.sleep(0.25)  # Wait for device to enable
+
+        # Get device in bay
+        device_type = rack.BayDeviceType(1)
+
+        if (device_type == ChannelDefinitions.ModularRackDevices.ModularRackPiezo):
+            piezo = rack.GetPiezoChannel(1)
+            if piezo is not None:
+                print("Piezo Connected.")
+
+
+        # Load any configuration settings needed by the controller/stage
+
+        # Get parameters related to homing/zeroing/other
+
+        # Home or Zero the device (if a motor/piezo)
+
+        # Move the device to a new position
+
+        # Stop Polling and Disconnect
+        print("Disconnecting")
+        device.StopPolling()
+        device.Disconnect()
+
+    except Exception as e:
+        # this can be bad practice: It sometimes obscures the error source
+        print(e)
+
+    # Uncomment this line if you are using Simulations
+    # SimulationManager.Instance.UninitializeSimulations()
+    ...
+
+
+if __name__ == "__main__":
+    main()

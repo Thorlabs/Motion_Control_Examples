f3de8007d3c0f423123a79031e28d99a137d2c21
Max Callé <95913120+ThorlabsMCalle@users.noreply.github.com>
2022-07-27 17:20:27 +0100

    BBD30x added timePos creation C++

--- C++/Benchtop/BBD30X_Synch_Move/BBD30X_Synch_Move.cpp
+++ C++/Benchtop/BBD30X_Synch_Move/BBD30X_Synch_Move.cpp
@@ -61,6 +61,65 @@ int main()
 			}
 
 			printf("Both Channels Homed");
+
+			// Set synchronous move parameters
+
+			WORD arrID = 1; // Like the Highlander, there can only be 1
+			WORD chanMask = 0x01 + 0x02; // mask for channels 1 and 2 (0x03 for channel 3)
+			WORD numPoints = 100; // 100 Total x/y positions
+			WORD startIndex = 0; // Only non-zero if using more than 256 positions
+
+			const WORD arrSize = numPoints * 300;
+			long * timePos = new long[arrSize]; // Necessary to make array legal
+
+			/*
+			* Synch positions accepted as 1D array of time and position
+			* These are in groups of 1 + numOfChannels, e.g. if using 2 channels, then
+			* each position is defined by a 3 value group:
+			* 0 100000 100000 
+			* 80 200000 200000 represents two moves
+			* 
+			* Positions are given in device units
+			* Times are given as time since last move in device time units
+			* 1 device time unit = 0.1024 ms
+			*/ 
+
+			WORD hundredMS = 977; // Approx 100 ms in device
+
+			// Get device params, including device units
+			MOT_StageAxisParameters chan1Params;
+			MOT_StageAxisParameters chan2Params;
+
+			BMC_GetStageAxisParamsBlock(testSerialNo, 1, &chan1Params);
+			BMC_GetStageAxisParamsBlock(testSerialNo, 2, &chan2Params);
+
+			int chan1DevUnit = chan1Params.countsPerUnit; // gets the device unit count per mm or degree
+			int chan2DevUnit = chan2Params.countsPerUnit; 
+
+			printf("chan 1 dev unit per mm: %d", chan1DevUnit);
+			printf("chan 2 dev unit per mm: %d", chan2DevUnit); 
+
+			int xHome = BMC_GetPosition(testSerialNo, 1);
+			int yHome = BMC_GetPosition(testSerialNo, 1); // in dev units
+
+			int chan1inc = chan1DevUnit / 10; // incremental move
+			int chan2inc = chan2DevUnit / 10;
+
+			// The following is just one way of building a large array with less code.
+			// It's not necessarily the best practice
+
+			timePos[0] = 0; // starting at 0 seconds
+			timePos[1] = xHome + chan1inc;
+			timePos[2] = yHome + chan2inc;
+
+			int j = 2; // starting with increment of 0.2mm
+			for (int i = 3; i < 300; i += 3) {
+				timePos[i] = hundredMS;
+				timePos[i + 1] = xHome + (chan1inc * j);
+				timePos[i + 2] = yHome + (chan2inc * j);
+				j++;
+			}
+
 		}
 
 	}
@@ -68,6 +127,7 @@ int main()
 
 	// Uncomment this line if you are using simulations
 	TLI_UninitializeSimulations();
+	printf("Program Finished. Press any key and Enter to exit.");
 	char c = _getch();
 }
 

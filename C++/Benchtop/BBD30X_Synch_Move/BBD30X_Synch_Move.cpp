a49e405d17eeef84266d52ce763d5503c995a5aa
Max Callé <95913120+ThorlabsMCalle@users.noreply.github.com>
2022-07-27 10:40:12 +0100

    Added initial homing code BBD30X Synch c++

--- C++/Benchtop/BBD30X_Synch_Move/BBD30X_Synch_Move.cpp
+++ C++/Benchtop/BBD30X_Synch_Move/BBD30X_Synch_Move.cpp
@@ -12,7 +12,59 @@ int main()
 	// Uncomment this line (and TLI_UnitializeSimulations at the bottom of the page)
 	// If you are using a simulated device
 	TLI_InitializeSimulations();
-    std::cout << "Hello World!\n";
+
+	// Change this line to reflect your device's serial number
+	int serialNo = 103000001;
+
+	// identify and access device
+	char testSerialNo[16];
+	sprintf_s(testSerialNo, "%d", serialNo);
+
+	if (TLI_BuildDeviceList() == 0) {
+		// get device list size 
+		short n = TLI_GetDeviceListSize(); // not used here, but useful
+
+		if (BMC_Open(testSerialNo) == 0) {
+			// device successfully opened, no error
+			// Begin polling on two channels and enable
+
+			BMC_StartPolling(testSerialNo, 1, 250);
+			BMC_StartPolling(testSerialNo, 2, 250);
+			Sleep(500); // Allow time for devices to respond
+
+			BMC_EnableChannel(testSerialNo, 1);
+			BMC_EnableChannel(testSerialNo, 2);
+			Sleep(500);
+
+			BMC_ClearMessageQueue(testSerialNo, 1);
+
+			// Home both channels
+			WORD messageType; // aka unsigned short
+			WORD messageId;
+			DWORD messageData; // aka unsigned long
+
+			printf("Channel 1 Homing");
+			BMC_Home(testSerialNo, 1);
+			// Wait for "Homing Complete" Message on channel 1
+			BMC_WaitForMessage(testSerialNo, 1, &messageType, &messageId, &messageData);
+			while (messageType != 2 || messageId != 0)
+			{
+				BMC_WaitForMessage(testSerialNo, 1, &messageType, &messageId, &messageData);
+			}
+
+			printf("Channel 2 Homing");
+			BMC_Home(testSerialNo, 1);
+			BMC_WaitForMessage(testSerialNo, 2, &messageType, &messageId, &messageData);
+			while (messageType != 2 || messageId != 0)
+			{
+				BMC_WaitForMessage(testSerialNo, 2, &messageType, &messageId, &messageData);
+			}
+
+			printf("Both Channels Homed");
+		}
+
+	}
+	// We'll not be setting velocity parameters or moving
 
 	// Uncomment this line if you are using simulations
 	TLI_UninitializeSimulations();

3f9cfa8784e642894115cc6b515b8acf8670986a
TrumanMetz <110424609+TrumanMetz@users.noreply.github.com>
2023-11-02 15:49:18 -0400

    Update kbd101_example.py
    Filled in rest of the example.

--- Python/KCube/KBD101/kbd101_example.py
+++ Python/KCube/KBD101/kbd101_example.py
@@ -19,7 +19,7 @@ def main():
     main():
     ------
 
-    Performs all actions of the KDC101
+    Performs all actions of the KBD101
     :return: None
     """
 
@@ -30,7 +30,60 @@ def main():
 
     lib: CDLL = cdll.LoadLibrary("Thorlabs.MotionControl.KCube.BrushlessMotor.dll")
 
-    return None
+
+    # Uncomment this line if you are using simulations
+    #lib.TLI_InitializeSimulations()
+
+    serial_num = c_char_p(b"00000000")
+
+    # Open the device.
+    if lib.TLI_BuildDeviceList() == 0: 
+        lib.BMC_Open(serial_num)
+        lib.BMC_StartPolling(serial_num, c_int(200))
+
+        # Home the device
+        lib.BMC_Home(serial_num)
+        time.sleep(1)
+
+        # Set up the device to convert real units to device units
+        STEPS_PER_REV = c_double(1919.64186)  
+        gbox_ratio = c_double(1.0)  
+        pitch = c_double(1.0)
+
+        # Get the device's current position in dev units
+        lib.BMC_RequestPosition(serial_num)
+        time.sleep(0.2)
+        dev_pos = c_int(lib.BMC_GetPosition(serial_num))
+
+        # Convert device units to real units
+        real_pos = c_double()
+        lib.BMC_GetRealValueFromDeviceUnit(serial_num,
+                                          real_pos,
+                                          byref(real_pos),
+                                          0)
+
+        print(f'Position after homing: {real_pos.value}')
+
+        # set a new position in device units
+        new_pos_real = c_double(5.0)  # in real units
+        new_pos_dev = c_int()
+        lib.BMC_GetDeviceUnitFromRealValue(serial_num,
+                                          new_pos_real,
+                                          byref(new_pos_dev),
+                                          0)
+
+        print(f'{new_pos_real.value} in Device Units: {new_pos_dev.value}')
+
+        # Move to new position as an absolute move.
+        lib.BMC_SetMoveAbsolutePosition(serial_num, new_pos_dev)
+        time.sleep(0.25)
+        lib.BMC_MoveAbsolute(serial_num)
+
+        # Close the device
+        lib.BMC_Close(serial_num)
+
+        # TLI_UninitializeSimulations()
+    return
 
 
 if __name__ == "__main__":

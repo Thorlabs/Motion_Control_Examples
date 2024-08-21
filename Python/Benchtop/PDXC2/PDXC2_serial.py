f798eca288af912463025aea25ae572c19a339d8
ziying zhang <zizhang@thorlabs.com>
2024-08-21 17:59:50 +0800

    PDXC2 python serial example

---
+++ Python/Benchtop/PDXC2/PDXC2_serial.py
@@ -0,0 +1,306 @@
+"""
+Example PDXC2_serial.py
+Example Date of Creation: 2024-08-08
+Example Date of Last Modification on Github: 2024-08-08
+Version of Python used for Testing: 3.10
+==================
+Example Description: The example shows how to connect to PDXC2 on Windows and Linux. 
+It also shows how to move the stage in open loop and close loop mode. 
+The close loop mode is only available for stages with encoder. 
+"""
+from struct import pack, unpack
+import serial
+import time
+
+serialPort = "COM4" #find out COM port number in the Device Manager
+openLoopStep = 0 #unit: step
+closeLoopPos = 0 #unit: nm, minimum step: 10 nm
+
+#Find section "4 Description of the message header" of the APT communication protocl PDF for detail
+#APT communication protocl PDF can be found https://www.thorlabs.com/software_pages/ViewSoftwarePage.cfm?Code=Motion_Control&viewtab=2
+destination = 0x50 #Destination Byte. 0x50 for generic USB hardware unit. 
+source = 0x01 #Source Byte
+channel = 0x01 #choose the channel to control. For PDXC2, the channel is set to 0x01
+
+def main():
+    
+    try:
+        # open serial port
+        # For Windows
+        ser=serial.Serial(port=serialPort ,baudrate=115200,bytesize=8,
+                        parity=serial.PARITY_NONE,stopbits=1,xonxoff=0,
+                        rtscts=0,timeout=1)
+        # For Linux
+        # use 'dmesg | grep tty' to find serial port, eventually need to change permissions for /dev/tty*
+        # uncomment the following lines
+        ##    ser=serial.Serial('/dev/ttyUSB0', baudrate=115200,bytesize=8,
+        ##                      parity=serial.PARITY_NONE,stopbits=1,xonxoff=0,
+        ##                      rtscts=0,timeout=1)  
+    except serial.SerialException as e:
+        print(f"Error opening serial port: {e}")
+        return -1
+    
+    try:
+        # Flush input and output buffer
+        ser.flushInput()
+        ser.flushOutput()
+        # Enable the stage
+        channelStatus = Enable(ser)
+        if channelStatus == 0:
+            print("Fail to enable the channel.")
+        elif channelStatus == 1:
+            OpenLoopMove(ser)
+            # The close loop mode is only valid for PDX series stages with encoder
+            CloseLoopMove(ser)  
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        ser.close()   
+        print("Program finishes.")
+        return 1
+
+
+def Enable(ser):
+    Rx = ser.read(90)
+	# Request Hardware information || MGMSG_HW_REQ_INFO || 0x0005
+    command = pack('<HBBBB',0x0005, 0x00, 0x00, destination, source)
+    ser.write(command)
+    time.sleep(0.05)
+    
+    # Get hardware information || MGMSG_HW_GET_INFO || 0x0006
+    Rx = ser.read(90)
+    if len(Rx) >= 10: 
+        serialNum = unpack('<I',Rx[6:10])[0]
+        print(f"Opening SN: {serialNum}")
+    else:
+        print("MGMSG_HW_GET_INFO: Fail to receive bytes. ")
+        return -1
+    
+    # Flush input and output buffer
+    ser.flushInput()
+    ser.flushOutput()
+    
+    # Request device status || MGMSG_PZMOT_REQ_STATUSUPDATE ||0x08E0
+    command = pack('<HBBBB',0x08E0, 0x00, 0x00, destination, source)
+    ser.write(command)
+    time.sleep(0.05)
+    
+    #Get device status of channel 1 || MGMSG_PZMOT_GET_STATUSUPDATE ||0x08E1
+    Rx = ser.read(62)
+    if len(Rx) >= 20:
+        statusBits = unpack('<L',Rx[16:20])[0]
+        # check if there are any errors in the controller's status.
+        if (statusBits & 0x0F000000)!= 0:
+            print(f"Status Error: 0x0{(statusBits& 0x0F000000):0x}")
+            print("""Status bits:
+    0x01000000 Excess current error
+    0x02000000 Exces temperature error
+    0x04000000 Abnormal mode detected
+    0x08000000 Wrong stage detected\n""")
+            return -1
+    else: 
+        print("MGMSG_PZMOT_GET_STATUSUPDATE: Fail to receive bytes.")
+        # Flush input and output buffer
+        ser.flushInput()
+        ser.flushOutput() 
+        return -1
+    
+    # Flush input and output buffer
+    ser.flushInput()
+    ser.flushOutput() 
+    
+    # enable the channel || MGMSG_MOD_SET_CHANENABLESTATE || 0x0210
+    command = pack('<HBBBB',0x0210, channel, 0x01, destination, source)
+    ser.write(command)
+    time.sleep(0.05)
+    
+    # request the channel status || MGMSG_MOD_REQ_CHANENABLESTATE ||0x0211
+    command = pack('<HBBBB',0x0211, channel, 0x00, destination, source)
+    ser.write(command)
+    time.sleep(0.05)
+    
+    # get the channel status || MGMSG_MOD_GET_CHANENABLESTATE ||0x0212
+    channelStatus = 0
+    Rx = ser.read(6)
+    if len(Rx) >= 3: 
+        channelStatus = unpack('<B',Rx[2:3])[0]
+    else:
+        print("MGMSG_MOD_GET_CHANENABLESTATE: Fail to receive bytes.")
+        return -1
+
+	# Flush input and output buffer
+    ser.flushInput()
+    ser.flushOutput()
+
+    return channelStatus
+    
+def OpenLoopMove(ser):
+    print("Open Loop Operation.")
+    # Set the operating loop mode to open loop || MGMSG_PZ_SET_POSCONTROLMODE  ||0x0640
+    command = pack('<HBBBB',0x0640, channel, 0x01, destination, source)
+    ser.write(command)
+    time.sleep(0.05)
+    
+    # Set the open loop move step size || MGMSG_PZMOT_SET_PARAMS (Set_PZMOT_OpenMoveParams) ||0x08C0 (sub-message ID 0x46)
+    command = pack('<HBBBBHHl',0x08C0, 0x08, 0x00, destination, source, 0x46, channel, openLoopStep)
+    ser.write(command)
+    time.sleep(0.05)
+    
+    #Start Open Loop Move || MGMSG_PZMOT_MOVE_START ||0x2100
+    command = pack('<HBBBB',0x2100, channel, 0x01, destination, source)
+    ser.write(command)
+    time.sleep(0.05)
+    
+    # Flush input and output buffer
+    ser.flushInput()
+    ser.flushOutput() 
+    
+    # Request device status || MGMSG_PZMOT_REQ_STATUSUPDATE ||0x08E0
+    command = pack('<HBBBB',0x08E0, 0x00, 0x00, destination, source)
+    ser.write(command)
+    time.sleep(0.05)
+    
+    #Get device status and position of channel 1 || MGMSG_PZMOT_GET_STATUSUPDATE ||0x08E1
+    Rx = ser.read(62)
+    if len(Rx) >= 20:
+        currentPos, encCount ,statusBits = unpack('<llL',Rx[8:20])
+        #wait for the movement to stop
+        while (statusBits & 0xF0) != 0 or Rx[0] != 0xE1:
+            # Flush input and output buffer
+            ser.flushInput()
+            ser.flushOutput() 
+            command = pack('<HBBBB',0x08E0, 0x00, 0x00, destination, source)
+            ser.write(command)
+            time.sleep(0.05)
+            Rx = ser.read(62)
+            currentPos, encCount ,statusBits = unpack('<llL',Rx[8:20])
+        print(f"The stage stops at {currentPos} steps.")
+        # Flush input and output buffer
+        ser.flushInput()
+        ser.flushOutput() 
+        return 1
+    else: 
+        print("MGMSG_PZMOT_GET_STATUSUPDATE: Fail to receive bytes.")
+        # Flush input and output buffer
+        ser.flushInput()
+        ser.flushOutput() 
+        return -1
+
+    
+def CloseLoopMove(ser):
+    print("Close Loop Operation.")
+    # Set the operating loop mode to close loop || MGMSG_PZ_SET_POSCONTROLMODE ||0x0640
+    command = pack('<HBBBB',0x0640, channel, 0x02, destination, source)
+    ser.write(command)
+    time.sleep(0.05)
+    
+    # Flush input and output buffer
+    ser.flushInput()
+    ser.flushOutput()
+    
+    # Request device status || MGMSG_PZMOT_REQ_STATUSUPDATE ||0x08E0
+    command = pack('<HBBBB',0x08E0, 0x00, 0x00, destination, source)
+    ser.write(command)
+    time.sleep(0.05)
+    
+    #Get device status of channel 1 || MGMSG_PZMOT_GET_STATUSUPDATE || 0x08E1
+    Rx = ser.read(62)
+    if len(Rx) >= 20:
+        statusBits = unpack('<L',Rx[16:20])[0]
+        # check if the stage is set to close loop mode
+        if (statusBits & 0x800)!= 0x800:
+            print("Fail to set the mode to close loop mode.")
+            return -1
+    else: 
+        print("MGMSG_PZMOT_GET_STATUSUPDATE: Fail to receive bytes.")
+        ser.flushInput()
+        ser.flushOutput() 
+        return -1
+    
+    # Flush input and output buffer
+    ser.flushInput()
+    ser.flushOutput()
+    
+    # home the stage || MGMSG_MOT_MOVE_HOME ||0x0443
+    command = pack('<HBBBB',0x0443, channel, 0x00, destination, source)
+    ser.write(command)
+    time.sleep(0.05)
+    
+    # Flush input and output buffer
+    ser.flushInput()
+    ser.flushOutput() 
+    
+    # Request device status || MGMSG_PZMOT_REQ_STATUSUPDATE ||0x08E0
+    command = pack('<HBBBB',0x08E0, 0x00, 0x00, destination, source)
+    ser.write(command)
+    time.sleep(0.05)
+    
+    #Get device status of channel 1 || MGMSG_PZMOT_GET_STATUSUPDATE ||0x08E1
+    Rx = ser.read(62)
+    if len(Rx) >= 20:
+        statusBits = unpack('<L',Rx[16:20])[0]
+        # wait home complete
+        while (statusBits & 0x400)!= 0x400 or Rx[0] != 0xE1:
+            # Flush input and output buffer
+            ser.flushInput()
+            ser.flushOutput() 
+            command = pack('<HBBBB',0x08E0, 0x00, 0x00, destination, source)
+            ser.write(command)
+            time.sleep(0.05)
+            Rx = ser.read(62)
+            statusBits = unpack('<L',Rx[16:20])[0]
+
+    else: 
+        print("MGMSG_PZMOT_GET_STATUSUPDATE: Fail to receive bytes.")
+        return -1
+        
+    print("The stage is Homed.")
+    
+    # Flush input and output buffer
+    ser.flushInput()
+    ser.flushOutput() 
+    
+    #Set Close Loop Move Parameter|| MGMSG_PZMOT_SET_PARAMS (Set_PZMOT_CloseMoveParams) ||0x08C0 (sub-message ID 0x47)
+    command = pack('<HBBBBHHl',0x08C0, 0x08, 0x00, destination, source, 0x47, channel, closeLoopPos)
+    ser.write(command)
+    time.sleep(0.05)
+    
+    #Start Close Loop Move || MGMSG_PZMOT_MOVE_START ||0x2100
+    command = pack('<HBBBB',0x2100, channel, 0x01, destination, source)
+    ser.write(command)
+    time.sleep(0.05)
+    
+    # Flush input and output buffer
+    ser.flushInput()
+    ser.flushOutput() 
+    
+    # Request device status || MGMSG_PZMOT_REQ_STATUSUPDATE ||0x08E0
+    command = pack('<HBBBB',0x08E0, 0x00, 0x00, destination, source)
+    ser.write(command)
+    time.sleep(0.05)
+    
+    #Get device status and position of channel 1 || MGMSG_PZMOT_GET_STATUSUPDATE ||0x08E1
+    Rx = ser.read(62)
+    if len(Rx) >= 20:
+        currentPos, encCount ,statusBits = unpack('<llL',Rx[8:20])
+        #wait for the movement to stop
+        while (statusBits & 0xF0) != 0 or Rx[0] != 0xE1:
+            # Flush input and output buffer
+            ser.flushInput()
+            ser.flushOutput() 
+            command = pack('<HBBBB',0x08E0, 0x00, 0x00, destination, source)
+            ser.write(command)
+            time.sleep(0.05)
+            Rx = ser.read(62)
+            currentPos, encCount ,statusBits = unpack('<llL',Rx[8:20])
+    else: 
+        print("MGMSG_PZMOT_GET_STATUSUPDATE: Fail to receive bytes.")
+        return -1
+    
+    print(f"The stage stops at {currentPos} nm.")
+
+
+if __name__ == "__main__":
+    main()
+
+
---
+++ Python/Benchtop/PDXC2/README.md
@@ -0,0 +1,20 @@
+# PDXC2 Examples
+Software examples for Thorlabs' PDXC2 Compact ORIC® Piezo Inertia Stage Controller
+
+## PDXC2_ctype.py
+This example uses the Kinesis C/C++ library and the Python module 'ctypes'.
+
+## PDXC2_pythonnet.py
+This example uses the Kinesis .NET library and the Python module 'pythonNET'.
+
+
+## PDXC2_serial.py
+
+This example uses serial commands as documented in the "APT Communications Protocol" and the Python module "pyserial". Please follow the following steps to setup a Virtual Communication Port for Windows OS. 
+1. Power up the device and connect the device to the PC using the USB cable
+2. Open the device manager by selecting Start/Control Panel/Device Manager/
+3. Click ‘USB Serial Bus Controllers’ and select the APT USB Device to be configured, then right click and select ‘Properties’.
+4. Select the ‘Advanced’ tab, and check the ‘Load VCP’ box.
+5. Click OK, then power cycle the device being configured.
+6. In the device manager, click ‘Ports (COM & LPT)’, and note the ‘APT USB Device Serial Port’ COM port number (e.g. COM3). This COM port can then be used for low level protocol messages.
+
